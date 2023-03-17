e9c811f677c6181bbb0ab5a8686d1d5031fc7b23
AhasanUllah <bulbulahasan@gmail.com>
2023-03-17 21:33:53 +0600

    rtk setup

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,16 @@
-import Video from "./Video";
+import { useGetVideosQuery } from '../../features/api/apiSlice';
+import Video from './Video';
+import Loading from '../../components/ui/loaders/VideoLoader';
+import Error from '../../components/ui/Error';
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
-    );
+   const { data: videos, isLoading, isError, error } = useGetVideosQuery();
+
+   let content;
+   if (isLoading) content = <Loading />;
+   if (!isLoading && isError) content = <Error message={'Something went wrong'} />;
+   if (!isLoading && !isError && videos.length === 0) content = <Error message={'no video found'} />;
+   if (!isLoading && !isError && videos.length > 0)
+      content = videos.map((video) => <Video key={video.id} video={video} />);
+   return <>{content}</>;
 }
